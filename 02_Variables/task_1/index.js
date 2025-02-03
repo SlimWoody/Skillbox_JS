66bedf1fb422b094e74446c394614c1bdb05d741
SlimWoody <125416780+SlimWoody@users.noreply.github.com>
2025-02-03 23:46:39 +0400

    Complete the tast 1

--- 02_Variables/task_1/index.js
+++ 02_Variables/task_1/index.js
@@ -1,10 +1,16 @@
 // Исправьте код этой программы так, что в консоль выводился результат выполнения программы, а не ошибка
-const currentYear = 2024;
+
+const currentYear = new Date().getFullYear();
 
 let nameStudent = 'Илья';
 let yearStudent = 2006;
 let ageStudent = currentYear - yearStudent;
 
-console.log('Студент:', nameStudent);
-console.log('Год рождения:', yearStudent);
-console.log('Возраст:', ageStudent);
+
+console.log(
+    `
+    Студент: ${nameStudent} \n
+    Год рождения: ${yearStudent} \n
+    Возраст ${ageStudent}
+    `
+);
