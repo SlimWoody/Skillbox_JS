708ade554be09ac9d456a20961421ee749ec1cd2
SlimWoody <125416780+SlimWoody@users.noreply.github.com>
2025-02-12 12:36:19 +0400

    Added HomeWork 3, task 2

---
+++ 03_Boolean_conditional_operators/task_2/index.js
@@ -0,0 +1,48 @@
+let carPower = +prompt('Введите мощность Вашего автомобиля в л.с :');
+let betUp;
+let transportTax;
+
+switch (true) {
+    case carPower < 100:
+        betUp = 12;
+        transportTax = carPower * betUp;
+        alert(`Сумма налога ${transportTax}`);
+        break;
+    case carPower > 100 && carPower < 125:
+        betUp = 25;
+        transportTax = carPower * betUp;
+        alert(`Сумма налога ${transportTax}`);
+        break;
+    case carPower > 125 && carPower < 150:
+        betUp = 35;
+        transportTax = carPower * betUp;
+        alert(`Сумма налога ${transportTax}`);
+        break;
+    case carPower > 150 && carPower < 175:
+        betUp = 45;
+        transportTax = carPower * betUp;
+        alert(`Сумма налога ${transportTax}`);
+        break;
+    case carPower > 175 && carPower < 200:
+        betUp = 50;
+        transportTax = carPower * betUp;
+        alert(`Сумма налога ${transportTax}`);
+        break;
+    case carPower > 200 && carPower < 225:
+        betUp = 65;
+        transportTax = carPower * betUp;
+        alert(`Сумма налога ${transportTax}`);
+        break;
+    case carPower > 225 && carPower < 250:
+        betUp = 75;
+        transportTax = carPower * betUp;
+        alert(`Сумма налога ${transportTax}`);
+        break;
+    case carPower > 250:
+        betUp = 150;
+        transportTax = carPower * betUp;
+        alert(`Сумма налога ${transportTax}`);
+        break;
+    default:
+        break;
+}
