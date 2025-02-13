aa9a0598b103e3aa116dbdcc85de7eae928cbfe6
SlimWoody <125416780+SlimWoody@users.noreply.github.com>
2025-02-14 00:48:36 +0400

    Added corrections HW-3, task 2 ver. 2

--- 03_Boolean_conditional_operators/task_2/index.js
+++ 03_Boolean_conditional_operators/task_2/index.js
@@ -1,48 +1,44 @@
 let carPower = +prompt('Введите мощность Вашего автомобиля в л.с :');
-let betUp;
-let transportTax;
 
-switch (true) {
-    case carPower < 100:
-        betUp = 12;
-        transportTax = carPower * betUp;
-        alert(`Сумма налога ${transportTax}`);
-        break;
-    case carPower >= 100 && carPower < 125:
-        betUp = 25;
-        transportTax = carPower * betUp;
-        alert(`Сумма налога ${transportTax}`);
-        break;
-    case carPower >= 125 && carPower < 150:
-        betUp = 35;
-        transportTax = carPower * betUp;
-        alert(`Сумма налога ${transportTax}`);
-        break;
-    case (carPower >= 150 && carPower) < 175:
-        betUp = 45;
-        transportTax = carPower * betUp;
-        alert(`Сумма налога ${transportTax}`);
-        break;
-    case carPower >= 175 && carPower < 200:
-        betUp = 50;
-        transportTax = carPower * betUp;
-        alert(`Сумма налога ${transportTax}`);
-        break;
-    case carPower >= 200 && carPower < 225:
-        betUp = 65;
-        transportTax = carPower * betUp;
-        alert(`Сумма налога ${transportTax}`);
-        break;
-    case carPower >= 225 && carPower < 250:
-        betUp = 75;
-        transportTax = carPower * betUp;
-        alert(`Сумма налога ${transportTax}`);
-        break;
-    case carPower >= 250:
-        betUp = 150;
-        transportTax = carPower * betUp;
-        alert(`Сумма налога ${transportTax}`);
-        break;
-    default:
-        break;
+// Проверка на верный ввод
+function isValid(item) {
+    return item === 0 || isNaN(item) || item === '' || item === null;
+}
+
+if (isValid(carPower)) {
+    alert('Данные некорректны');
+} else {
+    let betUp;
+    let transportTax;
+
+    switch (true) {
+        case carPower < 100:
+            betUp = 12;
+            break;
+        case carPower >= 100 && carPower < 125:
+            betUp = 25;
+            break;
+        case carPower >= 125 && carPower < 150:
+            betUp = 35;
+            break;
+        case carPower >= 150 && carPower < 175:
+            betUp = 45;
+            break;
+        case carPower >= 175 && carPower < 200:
+            betUp = 50;
+            break;
+        case carPower >= 200 && carPower < 225:
+            betUp = 65;
+            break;
+        case carPower >= 225 && carPower < 250:
+            betUp = 75;
+            break;
+        case carPower >= 250:
+            betUp = 150;
+            break;
+        default:
+            break;
+    }
+    transportTax = carPower * betUp;
+    alert(`Сумма налога ${transportTax}`);
 }
