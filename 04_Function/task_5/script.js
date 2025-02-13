4daef17b7212ef25f1eb6a9dbbdf9ec3a366ccc5
SlimWoody <125416780+SlimWoody@users.noreply.github.com>
2025-02-13 12:25:54 +0400

    Added Home Work 4, task 5

---
+++ 04_Function/task_5/script.js
@@ -0,0 +1,20 @@
+let priceItem = 25000;
+let amountItem = 3;
+let discountItem = 20;
+
+function calcTotalSum(price, amount, discount) {
+    let sumPrice = price * amount; // Вычисляем сумму покупки 
+    let sumDiscount = sumPrice * (discount / 100); // Вычесляем сумму скидки
+    let totalSum = sumPrice - sumDiscount // ВЫчисляем сумму покупки со скидкой
+    return totalSum;
+}
+
+let totalSum = calcTotalSum(priceItem, amountItem, discountItem);
+
+console.log(`Сумма со скидкой ${totalSum}`);
+
+// Сокращаем код 
+
+let newTotalSum = (price, amount, discount) => price * amount * (1 - discount / 100);
+console.log(`Сумма со скидкой ${newTotalSum(priceItem, amountItem, discountItem)}`);
+
