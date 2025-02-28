12303af05848ccd26d34a7ec8e4b08afe73ef259
SlimWoody <125416780+SlimWoody@users.noreply.github.com>
2025-02-28 11:21:07 +0400

    Changet hw 6 task 1 on forof loop

--- 06_Loop/task_1/script.js
+++ 06_Loop/task_1/script.js
@@ -1,27 +1,34 @@
+// Создаем массивы
 const currentProductArr = ['Мышка', 'Клавиатура', 'Наушники'];
 const newProductArr = ['Монитор', 'Принтер', 'Флешка'];
 
+// Находим элементы
 const productsListEl = document.querySelector('.products');
 const buttonAddProduct = document.querySelector('.products-add');
 
+// Создаем функцию добавления товара
 const addProduct = (productArr) => {
-    for (let i = 0; i < productArr.length; i++) {
-        const product = productArr[i];
+    for (const element of productArr) {
+        const product = element;
 
+        // Проверяем нет ли повторяющихся товаров
         const isProductExist = Array.from(productsListEl.children).some(
             (li) => li.textContent === product
         );
 
+        // Если повторений нет добавляем элемент с наименнование товара
         if (!isProductExist) {
             const liProductItem = document.createElement('li');
             liProductItem.classList.add('products__item');
-            liProductItem.textContent = productArr[i];
+            liProductItem.textContent = element;
 
             productsListEl.append(liProductItem);
         }
     }
 };
 
+// Вызов функции для загрузки текущих товаров
 addProduct(currentProductArr);
 
+// Добавил кнопку для добавлния новых товаров через обработчик событий.
 buttonAddProduct.addEventListener('click', () => addProduct(newProductArr));
